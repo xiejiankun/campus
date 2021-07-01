080dcda55ae91534859d78c7aa6d95bac898bbff
杨磊爸爸 <2632366503@qq.com>
2021-07-01 23:27:33 +0800

    index的html和css

--- src/main/java/com/jaken/secondHand/pojo/User.java
+++ src/main/java/com/jaken/secondHand/pojo/User.java
@@ -6,8 +6,6 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * <p>
